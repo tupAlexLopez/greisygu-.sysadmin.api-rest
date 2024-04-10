59e760e327acfa7ee4c1396d5411babc2aee5861
alex <alexander.lopez@awsoftware.com.ar>
2024-04-10 17:23:13 -0300

    update: Elimine Dockerfile y removi del gitignore la carpeta target, para testear el deploy de railway

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM maven:3.8.5-openjdk-17-slim as buildApp
-COPY . .
-RUN mvn clean package -DskipTests
-
-FROM openjdk:17-alpine
-COPY --from=buildApp "target/*.jar" "spring-crud-api-rest.jar"
-EXPOSE 8080
-
-ENTRYPOINT ["java","-jar","spring-crud-api-rest.jar"]
---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category(id, name) VALUES(1, 'Pastas');
+INSERT INTO category(id, name) VALUES(2, 'Bebidas');
+
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(1, '', 'Fideos tallarin', 10, 1, 10,1);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(2, '', 'Arroz blanco largo fino', 8, 1, 10,1);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(3, '', 'Cerveza brahma dorada', 35, 1, 10,2);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(4, '', 'Gaseosa de Pomelo', 20, 1, 10,2);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(5, '', 'Ravioles Cuatro Queso', 10, 1, 10,1);
