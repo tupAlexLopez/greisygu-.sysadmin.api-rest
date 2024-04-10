280b4b1dda8e1e3905511549830f3ada7e3014fb
alex <alexander.lopez@awsoftware.com.ar>
2024-04-10 11:01:15 -0300

    add: Cambios en archivo de configuracion y creacion de datos de prueba

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category(id, name) VALUES(1, 'Pastas');
+INSERT INTO category(id, name) VALUES(2, 'Bebidas');
+
+INSERT INTO product(id, name, description, price, category_id) VALUES(1, 'La providencia Tallarin','Fideos tallarin', 10, 1);
+INSERT INTO product(id, name, description, price, category_id) VALUES(2, 'Molinos Ala Arroz','Arroz blanco largo fino', 8, 1);
+INSERT INTO product(id, name, description, price, category_id) VALUES(3, 'Brahma 1.2Lts','Cerveza brahma dorada', 35, 2);
+INSERT INTO product(id, name, description, price, category_id) VALUES(4, 'Secco 3Lts','Gaseosa de Pomelo', 20, 2);
+INSERT INTO product(id, name, description, price, category_id) VALUES(5, 'Ravioles Cuatro Queso la Sateña','Ravioles Cuatro Queso', 10, 1);
