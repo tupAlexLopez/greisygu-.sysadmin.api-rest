4121fdc1629baf67dd5fa1d2d4336fb9981d1783
alex <alexander.lopez@awsoftware.com.ar>
2024-04-10 14:47:47 -0300

    update: Modificacion de datos de prueba

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO category(id, name) VALUES(1, 'Pastas');
 INSERT INTO category(id, name) VALUES(2, 'Bebidas');
 
-INSERT INTO product(id, name, description, price, category_id) VALUES(1, 'La providencia Tallarin','Fideos tallarin', 10, 1);
-INSERT INTO product(id, name, description, price, category_id) VALUES(2, 'Molinos Ala Arroz','Arroz blanco largo fino', 8, 1);
-INSERT INTO product(id, name, description, price, category_id) VALUES(3, 'Brahma 1.2Lts','Cerveza brahma dorada', 35, 2);
-INSERT INTO product(id, name, description, price, category_id) VALUES(4, 'Secco 3Lts','Gaseosa de Pomelo', 20, 2);
-INSERT INTO product(id, name, description, price, category_id) VALUES(5, 'Ravioles Cuatro Queso la Sateña','Ravioles Cuatro Queso', 10, 1);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(1, '', 'Fideos tallarin', 10, 1, 10,1);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(2, '', 'Arroz blanco largo fino', 8, 1, 10,1);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(3, '', 'Cerveza brahma dorada', 35, 1, 10,2);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(4, '', 'Gaseosa de Pomelo', 20, 1, 10,2);
+INSERT INTO product(id, url_img, description, price, available , stock ,category_id) VALUES(5, '', 'Ravioles Cuatro Queso', 10, 1, 10,1);
