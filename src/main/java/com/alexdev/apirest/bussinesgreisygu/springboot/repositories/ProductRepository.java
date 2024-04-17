622e02d1c4ae89c1ec0bd1dc9718a250aabeb6c4
tupAlexLopezz <tupalexlopez10@gmail.com>
2024-04-17 12:27:37 -0300

    add: Metodo para buscar por descripcion de producto en repositorio

--- src/main/java/com/alexdev/apirest/bussinesgreisygu/springboot/repositories/ProductRepository.java
+++ src/main/java/com/alexdev/apirest/bussinesgreisygu/springboot/repositories/ProductRepository.java
@@ -3,5 +3,10 @@ package com.alexdev.apirest.bussinesgreisygu.springboot.repositories;
 import com.alexdev.apirest.bussinesgreisygu.springboot.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> { }
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByDescription(String description);
+    List<Product> findByDescriptionContaining(String description);
+}
