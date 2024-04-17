d055c8ede5009c48d66c07947b79ee1884a381eb
tupAlexLopezz <tupalexlopez10@gmail.com>
2024-04-17 12:27:58 -0300

    add: Nuevo datos de prueba

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO category(id, name) VALUES(1, 'Pastas');
+INSERT INTO category(id, name) VALUES(2, 'Bebidas');
+
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Arroz blanco San Jose 500g', 1300, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Fideos Marolio 500g', 1250, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Secco Pomelo 3Lts', 1500, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Brahma 1.25Lts', 2400, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Heineken 1.25Lts', 2300, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Arroz blanco San Jose 500g', 1300, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Fideos Marolio 500g', 1250, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Secco Pomelo 3Lts', 1500, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Brahma 1.25Lts', 2400, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Heineken 1.25Lts', 2300, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Arroz blanco San Jose 500g', 1300, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Fideos Marolio 500g', 1250, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Secco Pomelo 3Lts', 1500, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Brahma 1.25Lts', 2400, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Heineken 1.25Lts', 2300, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Arroz blanco San Jose 500g', 1300, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Fideos Marolio 500g', 1250, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Secco Pomelo 3Lts', 1500, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Brahma 1.25Lts', 2400, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Heineken 1.25Lts', 2300, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Arroz blanco San Jose 500g', 1300, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Fideos Marolio 500g', 1250, 1,1);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Secco Pomelo 3Lts', 1500, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Brahma 1.25Lts', 2400, 1,2);
+INSERT INTO product(img, description, price, available , category_id) VALUES('', 'Heineken 1.25Lts', 2300, 1,2);
