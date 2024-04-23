426a1f9975c99c45257dd77c528440237e9fe5de
tupAlexLopezz <tupalexlopez10@gmail.com>
2024-04-22 21:58:23 -0300

    add: Creacion de mediaController para subida de imagenes

--- src/main/java/com/alexdev/apirest/bussinesgreisygu/springboot/controllers/MediaController.java
+++ src/main/java/com/alexdev/apirest/bussinesgreisygu/springboot/controllers/MediaController.java
@@ -1,19 +1,19 @@
 package com.alexdev.apirest.bussinesgreisygu.springboot.controllers;
 
+import com.alexdev.apirest.bussinesgreisygu.springboot.models.response.MediaResponse;
 import com.alexdev.apirest.bussinesgreisygu.springboot.services.UploadFileService;
-import jdk.jfr.ContentType;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/media")
@@ -21,19 +21,53 @@ public class MediaController {
     @Autowired
     UploadFileService uploadService;
 
-    @GetMapping("/{filename:.+}")
-    public ResponseEntity<byte[]> getMedia(@PathVariable String filename) throws IOException {
-        Resource resource = uploadService.search( filename );
-        Path path = Paths.get(resource.getURI());
-        byte[] imageData = Files.readAllBytes(path);
+    @Autowired HttpServletRequest req;
 
-        return ResponseEntity.ok()
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(imageData);
+    @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.OK)
+    public MediaResponse saveImageProduct(@RequestParam("file") MultipartFile file) throws IOException {
+        String filename =uploadService.store( file );
+        return MediaResponse.builder()
+                .url( getUrlImage( filename ) )
+                .filename( filename )
+                .build();
     }
+
+    @PutMapping("/{filename:.+}")
+    public MediaResponse updateImageProduct(@PathVariable String filename, @RequestParam MultipartFile file ) {
+        String newFilename =uploadService.update( filename, file );
+        return MediaResponse.builder()
+                .url( getUrlImage( newFilename ) )
+                .filename( newFilename )
+                .build();
+    }
+
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> getMedia(@PathVariable String filename) throws IOException {
+        Resource resource = uploadService.search( filename );
+        String contentType =Files.probeContentType( resource.getFile().toPath() );
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_TYPE, contentType)
+                .body( resource );
+    }
+
     @DeleteMapping("/{filename:.+}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteMedia(@PathVariable String filename) {
         uploadService.delete( filename );
+    }
+
+
+
+    private String getUrlImage( String filename ){
+        String host =req.getRequestURL().toString().replace( req.getRequestURI(), "");
+
+        return ServletUriComponentsBuilder
+                .fromHttpUrl( host )
+                .path("/media/")
+                .path( filename )
+                .toUriString();
     }
 }
