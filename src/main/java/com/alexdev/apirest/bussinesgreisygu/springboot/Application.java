ddae0789c7d186d0cee521ea9d88685290f61aae
tupAlexLopezz <tupalexlopez10@gmail.com>
2024-04-22 21:57:49 -0300

    remove: Removi uploadService del Main.

--- src/main/java/com/alexdev/apirest/bussinesgreisygu/springboot/Application.java
+++ src/main/java/com/alexdev/apirest/bussinesgreisygu/springboot/Application.java
@@ -7,16 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-	@Autowired
-	UploadFileService uploadService;
-
+public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		uploadService.init();
 	}
 }
