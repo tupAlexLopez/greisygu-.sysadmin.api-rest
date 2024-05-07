5340eff99ec2ab90795ae2c27595f5b0a7aec57d
tupAlexLopez <tupalexlopez10@gmail.com>
2024-05-07 16:28:30 -0300

    delete: data.sql y ApplicationTests.java (sin uso)

--- src/main/resources/sql/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO category(name) VALUES('Pastas');
-INSERT INTO category(name) VALUES('Bebidas');
-
-INSERT INTO product(description, price, available, category_id) VALUES('Secco de Pomelo 3Lts', 1250, 1, 2);
-INSERT INTO product(description, price, available, category_id) VALUES('Cabalgata de Cola 2.25Lts', 1100, 1, 2);
-INSERT INTO product(description, price, available, category_id) VALUES('La Salteña Ravioles 450grs', 1300, 1, 1);
-INSERT INTO product(description, price, available, category_id) VALUES('La Providencia Mostachol 500grs', 950, 1, 1);
-INSERT INTO product(description, price, available, category_id) VALUES('Don Ernesto Arroz 1Kg', 1800, 1, 1);
